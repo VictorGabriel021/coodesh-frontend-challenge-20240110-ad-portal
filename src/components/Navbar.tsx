6dae8b0b3d99529f7948e633baf00608e7efc0d9
VictorGabriel021 <victorgabriel.cipa@gmail.com>
2024-05-11 00:05:29 -0300

    feat: add Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-import logo from "@/assets/logo.png";
+import Logo from "@/assets/img/LogoNav.png";
 
 import {
   Disclosure,
@@ -8,30 +8,30 @@ import {
   DisclosurePanel,
 } from "@headlessui/react";
 
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { MdClose } from "react-icons/md";
+import { FaBars } from "react-icons/fa";
 
-import ArrowDown from "@/assets/icons/ArrowDown";
+import { IoIosArrowDown } from "react-icons/io";
 
 const navigation = [
-  { name: "Imobiliárias", href: "#", current: true },
-  { name: "Quero me Associar", href: "#", current: false },
+  { name: "Imobiliárias", href: "#" },
+  { name: "Quero me Associar", href: "#" },
   {
     name: (
       <div className="flex items-center gap-x-1">
         Sobre
-        <ArrowDown />
+        <IoIosArrowDown size={18} />
       </div>
     ),
     href: "#",
-    current: false,
   },
-  { name: "Blog", href: "#", current: false },
+  { name: "Blog", href: "#" },
 ];
 
 const navigationMobile = [
   ...navigation,
-  { name: "Anunciar Imóvel", href: "#", current: false },
-  { name: "Entrar", href: "#", current: false },
+  { name: "Anunciar Imóvel", href: "#" },
+  { name: "Entrar", href: "#" },
 ];
 
 const Navbar = () => {
@@ -39,9 +39,9 @@ const Navbar = () => {
     <Disclosure as="nav">
       {({ open }) => (
         <>
-          <div className="fixed top-0 bg-white flex items-center justify-between w-full space-x-10 py-3 px-5 lg:m-0 lg:mx-auto max-w-screen-xl">
+          <div className="fixed top-0 z-10 bg-white flex items-center justify-between w-full space-x-10 py-3 px-5 lg:m-0 lg:mx-auto mb-[4em] max-w-[1440px]">
             <a href="#" className="mr-3">
-              <Image src={logo} alt="Netimóveis" />
+              <Image src={Logo} alt="Netimóveis" height={42} />
             </a>
 
             <div className="hidden lg:block">
@@ -53,7 +53,6 @@ const Navbar = () => {
                     className={
                       "text-custom-gray-555555 hover:bg-custom-orange-E95808 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
                     }
-                    aria-current={item.current ? "page" : undefined}
                   >
                     {item.name}
                   </a>
@@ -63,11 +62,7 @@ const Navbar = () => {
 
             <div className="lg:hidden">
               <DisclosureButton className="p-2 text-custom-gray-555555 hover:bg-custom-orange-E95808 hover:text-white">
-                {open ? (
-                  <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-                ) : (
-                  <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-                )}
+                {open ? <MdClose size={24} /> : <FaBars size={24} />}
               </DisclosureButton>
             </div>
 
@@ -85,7 +80,7 @@ const Navbar = () => {
           </div>
 
           <DisclosurePanel className="lg:hidden">
-            <div className="fixed w-full mt-[4em] space-y-1 px-2 pb-3 pt-2">
+            <div className="fixed z-10 w-full top-16 space-y-1 px-2 pb-3 pt-2">
               {navigationMobile.map((item, index) => (
                 <DisclosureButton
                   key={index}
@@ -94,7 +89,6 @@ const Navbar = () => {
                   className={
                     "text-white bg-custom-orange-F36C21 hover:bg-custom-orange-E95808 block rounded-md px-3 py-2"
                   }
-                  aria-current={item.current ? "page" : undefined}
                 >
                   {item.name}
                 </DisclosureButton>
